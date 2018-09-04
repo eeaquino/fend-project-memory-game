1f40c57c1e5f9247c98271e6d3567216012438de
Edgar Aquino <eeaquino@hotmail.com>
2018-09-03 22:04:15 -0700

    Added code for animations and transitions.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 /*
  * Create a list that holds all of your cards
  */
-
-
+const deck = document.getElementById("deck");
+const cards = deck.getElementsByClassName("card");
+let animating = false;
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+shuffle(cards);
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -36,3 +39,77 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+/* Events*/
+deck.addEventListener("click", (e) =>
+{
+    let clickedCard = e.target.parentElement;
+    if (clickedCard.classList.contains("card")) {
+        openCard(clickedCard).then(() =>
+        {
+            if (countOpen() === 2) {
+                if (validatePair()) {
+                    //TODO
+                } else {
+                    shakeOpenCards().then(() =>
+                    {
+                        closeAllOpenCards();
+                    });
+                }
+            }
+        });
+    }
+});
+/*Animations and transitions*/
+function shakeOpenCards()
+{
+    return new Promise(function(resolve) {
+        let openCards = deck.getElementsByClassName("opened");
+        for (let card of openCards) {
+            card.classList.add("noMatch");
+        }
+        openCards[0].addEventListener("animationend",
+            function _tempShakeEnd(e)
+            {
+                openCards[0].removeEventListener(e.type, _tempShakeEnd,false);
+                resolve(true);
+            });
+    });
+    
+}
+function closeAllOpenCards()
+{
+    let openCards = deck.getElementsByClassName("opened");
+    while (openCards.length > 0) {
+        closeCard(openCards[0]);
+    }
+}
+function openCard(card)
+{
+    return new Promise(function(resolve) {
+        card.classList.add("open", "show");
+        card.addEventListener("transitionend",
+            function _tempTranEnd(e)
+            {
+                card.removeEventListener(e.type, _tempTranEnd,false);
+                card.classList.remove("open");
+                card.classList.add("opened");
+                resolve(true);
+            });
+    });
+    
+}
+function closeCard(card)
+{
+    card.classList.remove("opened", "show","noMatch");
+}
+/*Utilities */
+function countOpen()
+{
+    let openCards = deck.getElementsByClassName("opened");
+    return openCards.length;
+}
+/*Logic*/
+function validatePair()
+{
+    return false;
+}
